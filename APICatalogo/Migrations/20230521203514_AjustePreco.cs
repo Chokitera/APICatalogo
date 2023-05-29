a7e9d2e5682454221041b39a53375a87d82af8c1
alan alves de macedo <alan_chocoleite10@hotmail.com>
2023-05-28 21:22:39 -0300

    Adicionar arquivos de projeto.

---
+++ APICatalogo/Migrations/20230521203514_AjustePreco.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class AjustePreco : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "preco",
+                table: "Produtos",
+                newName: "Preco");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Preco",
+                table: "Produtos",
+                newName: "preco");
+        }
+    }
+}
